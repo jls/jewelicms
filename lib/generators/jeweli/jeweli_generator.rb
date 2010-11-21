7408ea7409de790fa1a697ece9a3a73003586566
James Smith <jlsmith@gmail.com>
2010-11-21 15:05:09 -0500

    Updated generator for Rails 3

--- lib/generators/jeweli/jeweli_generator.rb
+++ lib/generators/jeweli/jeweli_generator.rb
@@ -1,32 +1,30 @@
-class JeweliGenerator < Rails::Generator::NamedBase
-  attr_reader :layout_name
-  def initialize(runtime_args, runtime_options = {})
-    super
-    @layout_name =  name
+class JeweliGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+  argument :layout_name, :type => :string, :default => 'jewelicms.com'
+  
+  def generate_layout
+  
+    # Application layout
+    copy_file "#{layout_name}/layout.html.erb", 'app/views/layouts/application.html.erb'
+    
+    # Database seeds
+    copy_file "#{layout_name}/seeds.rb", "db/seeds_#{layout_name}.rb"
+    
+    # Images
+    Dir[File.join(File.dirname(__FILE__),"templates", layout_name, "images", "*.*")].sort.each { |i| 
+      copy_file("#{layout_name}/images/#{File.basename(i)}", "public/images/#{File.basename(i)}")
+    }
+    
+    # Stylesheets
+    Dir[File.join(File.dirname(__FILE__),"templates", layout_name, "stylesheets", "*.*")].sort.each { |i| 
+      copy_file("#{@layout_name}/stylesheets/#{File.basename(i)}", "public/stylesheets/#{File.basename(i)}")
+    }
+    
+    # Templates
+    Dir[File.join(File.dirname(__FILE__),"templates", layout_name, "templates", "*.*")].sort.each { |i| 
+      copy_file("#{layout_name}/templates/#{File.basename(i)}", "app/views/templates/#{File.basename(i)}")
+    }
+    
   end
   
-  def manifest
-    record do |m|
-      # Application layout
-      m.file("#{@layout_name}/layout.html.erb", 'app/views/layouts/application.html.erb')
-      
-      # Database seeds
-      m.file("#{@layout_name}/seeds.rb", "db/seeds_#{@layout_name}.rb")
-      
-      # Images
-      Dir[File.join(File.dirname(__FILE__),"templates", @layout_name, "images", "*.*")].sort.each { |i| 
-        m.file("#{@layout_name}/images/#{File.basename(i)}", "public/images/#{File.basename(i)}")
-      }
-      
-      # Stylesheets
-      Dir[File.join(File.dirname(__FILE__),"templates", @layout_name, "stylesheets", "*.*")].sort.each { |i| 
-        m.file("#{@layout_name}/stylesheets/#{File.basename(i)}", "public/stylesheets/#{File.basename(i)}")
-      }
-      
-      # Templates
-      Dir[File.join(File.dirname(__FILE__),"templates", @layout_name, "templates", "*.*")].sort.each { |i| 
-        m.file("#{@layout_name}/templates/#{File.basename(i)}", "app/views/templates/#{File.basename(i)}")
-      }
-    end
-  end
 end
